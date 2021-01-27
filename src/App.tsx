8c3fc3492edf26544908f357d504b4c3ea46e62c
die-goncalves <die.goncalves1990@gmail.com>
2021-01-26 22:13:09 -0300

    Create the homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
 import React from "react";
+import logoImg from "./images/logo.svg";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div id="homepage">
+      <div className="grid-container">
+        <div className="logo"></div>
+        <div className="city">
+          <strong>Jaguaré</strong>
+          <span>Espírito Santo</span>
+        </div>
+        <div className="restrict">Acesso restrito</div>
+        <div className="map">mapa</div>
+        <div className="img"></div>
+        <div className="description">
+          <p>Visite orfanatos e mude o dia de muitas crianças!</p>
+        </div>
+        <div className="title">
+          <h1>
+            Leve
+            <br />
+            felicidade
+            <br />
+            para
+            <br />o mundo
+          </h1>
+        </div>
+      </div>
     </div>
   );
 }
