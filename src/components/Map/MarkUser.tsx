6fadf1389592481be456f1114be09c4c0bcd494e
die-goncalves <die.goncalves1990@gmail.com>
2021-02-03 11:49:50 -0300

    Add component to mark user
    -> Create icon to be shown on the map.
    -> Use <Popup> component to display marker information.
    -> Use <Circle> component to show a circle where the user could be.

---
+++ src/components/Map/MarkUser.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import L, { LatLngExpression } from "leaflet";
+import { Circle, Marker, Popup } from "react-leaflet";
+import youMarker from "../../images/you-better.svg";
+import youShadowMarker from "../../images/you-better-shadow.svg";
+
+const mapIcon = L.icon({
+  iconUrl: youMarker,
+  shadowUrl: youShadowMarker,
+  iconSize: [47.0829, 63.00583],
+  iconAnchor: [23.54145, 63.00583],
+  shadowSize: [53.5373, 33.6722],
+  shadowAnchor: [0, 33.6722],
+  popupAnchor: [0.25, -63.00583],
+});
+
+type LocationParams = {
+  mylocation: GeolocationPosition;
+};
+
+export default function MarkUser({ mylocation }: LocationParams) {
+  const latlng: LatLngExpression = [
+    mylocation.coords.latitude,
+    mylocation.coords.longitude,
+  ];
+  return (
+    <Marker position={latlng} icon={mapIcon}>
+      <Popup>
+        <h1>You are here.</h1>
+        <p>
+          lat:{latlng[0]} - lng:{latlng[1]}
+        </p>
+      </Popup>
+      <Circle
+        center={latlng}
+        pathOptions={{
+          fillColor: "#f9fafb",
+          fillOpacity: 0.3,
+          stroke: true,
+          color: "#0d4e6b",
+          weight: 1.5,
+        }}
+        radius={mylocation.coords.accuracy}
+      />
+    </Marker>
+  );
+}
