8de679057cbf8fdabf7fdd18ea97a4d25db85da9
die-goncalves <die.goncalves1990@gmail.com>
2021-02-08 22:04:59 -0300

    Create route to the page of register orphanages

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,13 +3,15 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Homepage from "./screens/Homepage";
 import Mappage from "./screens/Map";
+import Createpage from "./screens/Create";
 
 function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Homepage} />
-        <Route path="/map" component={Mappage} />
+        <Route exact path="/map" component={Mappage} />
+        <Route exact path="/map/create" component={Createpage} />
       </Switch>
     </BrowserRouter>
   );
