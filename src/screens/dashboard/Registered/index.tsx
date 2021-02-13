ae25e0e64c7ac4608bedf88cf0f8c4890fee4a19
die-goncalves <die.goncalves1990@gmail.com>
2021-02-13 10:00:49 -0300

    Update dashboard/registered page
    -> Add the SidebarDashboard component;
    -> Add the OrfanatoCard component;
    -> Add text where you will have the number of orphanages.

--- src/screens/dashboard/Registered/index.tsx
+++ src/screens/dashboard/Registered/index.tsx
@@ -1,7 +1,35 @@
 import React from "react";
+import SidebarDashboard from "../../../components/dashboard/SidebarDashboard";
+import OrfanatoCard from "../../../components/dashboard/OrfanatoCard";
+import "./styles.css";
 
 function Registeredpage() {
-  return <div>orfanatos cadastrados</div>;
+  return (
+    <div id="registered-page">
+      <SidebarDashboard />
+      <div id="registered-page-main">
+        <div className="registered-page-header">
+          <p>Orfanatos cadastrados</p>
+          <span>x orfanatos</span>
+        </div>
+        <div className="registered-page-cluster">
+          <div className="registered-page-grid-container">
+            <OrfanatoCard />
+            <OrfanatoCard />
+            <OrfanatoCard />
+            <OrfanatoCard />
+            <OrfanatoCard />
+            <OrfanatoCard />
+            <OrfanatoCard />
+            <OrfanatoCard />
+            <OrfanatoCard />
+            <OrfanatoCard />
+            <OrfanatoCard />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Registeredpage;
