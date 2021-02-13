b63ca5429758e30a6b4e7a03aed60eea2b14c388
die-goncalves <die.goncalves1990@gmail.com>
2021-02-13 10:51:34 -0300

    Update dashboard/pending page
    -> Add the SidebarDashboard component;
    -> Add the OrfanatoCardPending component;
    -> Add text where you will have the number of orphanages.

--- src/screens/dashboard/Pending/index.tsx
+++ src/screens/dashboard/Pending/index.tsx
@@ -1,8 +1,34 @@
 import React from "react";
+import SidebarDashboard from "../../../components/dashboard/SidebarDashboard";
+import OrfanatoCardPending from "../../../components/dashboard/OrfanatoCardPending";
 import "./styles.css";
 
 function Pendingpage() {
-  return <div id="pending-page">Cadastros pendentes</div>;
+  return (
+    <div id="pending-page">
+      <SidebarDashboard />
+      <div id="pending-page-main">
+        <div className="pending-page-header">
+          <p>Cadastros pendentes</p>
+          <span>x orfanatos</span>
+        </div>
+        <div className="pending-page-cluster">
+          <div className="pending-page-grid-container">
+            <OrfanatoCardPending />
+            <OrfanatoCardPending />
+            <OrfanatoCardPending />
+            <OrfanatoCardPending />
+            <OrfanatoCardPending />
+            <OrfanatoCardPending />
+            <OrfanatoCardPending />
+            <OrfanatoCardPending />
+            <OrfanatoCardPending />
+            <OrfanatoCardPending />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Pendingpage;
