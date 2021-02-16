25f52b606f1e73478bd4ec4c7fad7e2d8c4e8c88
die-goncalves <die.goncalves1990@gmail.com>
2021-02-16 14:42:33 -0300

    Update orphanage detail page
    -> Add SideBarPublic component and use the rest of the page.
    -> Create a space for the future creation of a loading to be shown until you have the response from the backend.
    -> Create an area to highlight an image.
    -> Create an area to show the images in a row with space for three images at a time. The images will be alternated through two buttons on the left and right side located at the end of each side of the area and centrally simulating a horizontal scroll. By clicking on the buttons we have a slideshow effect where the highlighted image will alternate, we can also click on an image so it will be in focus and will be shown in the highlighted image.
    -> Use the information provided by the backend to show all relevant information from an orphanage.
    Note: Problem when trying to access the images saved locally through the frontend, there is a security error. So I changed the backend to save the images in the '/public/uploads' folder on the frontend, so I can access them.

--- src/screens/Detail/index.tsx
+++ src/screens/Detail/index.tsx
@@ -1,9 +1,38 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-
 import "./styles.css";
 import "leaflet/dist/leaflet.css";
 import api from "../../services/api";
+import { MapContainer, Marker, TileLayer } from "react-leaflet";
+import L from "leaflet";
+import mapMarker from "../../images/map-marker.svg";
+import { FiClock, FiInfo, FiArrowRight, FiArrowLeft } from "react-icons/fi";
+import SidebarPublic from "../../components/SidebarPublic";
+
+function sideScroll(
+  element: HTMLElement,
+  direction: string,
+  speed: number,
+  distance: number,
+  step: number
+) {
+  /*
+    horizontal scroll with buttons based here:
+      https://jsfiddle.net/pGR3B/2/
+  */
+  let scrollAmount = 0;
+  var slideTimer = setInterval(function () {
+    if (direction == "left") {
+      element.scrollLeft -= step;
+    } else {
+      element.scrollLeft += step;
+    }
+    scrollAmount += step;
+    if (scrollAmount >= distance) {
+      window.clearInterval(slideTimer);
+    }
+  }, speed);
+}
 
 interface Orphanage {
   latitude: number;
@@ -14,7 +43,9 @@ interface Orphanage {
   opening_hours: string;
   open_on_weekends: boolean;
   pictures: Array<{
-    url: string;
+    _id: any;
+    destination: string;
+    filename: string;
   }>;
 }
 
@@ -22,11 +53,18 @@ interface OrphanageParams {
   _id: any;
 }
 
+const mapIcon = L.icon({
+  iconUrl: mapMarker,
+
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [0, -60],
+});
+
 function Detailpage() {
-  const [orphanage, setOrphanage] = useState<Orphanage[]>([]);
+  const [orphanage, setOrphanage] = useState<Orphanage>();
+  const [activeImageIndex, setActivateImageIndex] = useState(0);
   const params = useParams<OrphanageParams>();
-  // console.log(watch("pictures"));
-  console.log(params._id);
 
   useEffect(() => {
     api.get(`hosting/show?_id=${params._id}`).then((response: any) => {
@@ -34,10 +72,141 @@ function Detailpage() {
     });
   }, [params._id]);
 
+  if (!orphanage) {
+    return <p>Carregando...</p>;
+  }
+
   return (
     <div id="detail-page">
-      Detalhes
-      <p> Seus parametros {params._id}</p>
+      <SidebarPublic />
+
+      <div className="detail-page-rightside">
+        <div className="detail-page-orphanage">
+          <img
+            className="detail-page-picture-focus"
+            src={`/uploads/${orphanage.pictures[activeImageIndex].filename}`}
+            // src={`${orphanage.pictures[activeImageIndex].destination}/${orphanage.pictures[activeImageIndex].filename}`}
+            alt={orphanage.name}
+          />
+
+          <div className="detail-page-below-picture">
+            <div className="detail-page-nome-descricao">
+              <h1>{orphanage.name}</h1>
+              <p>{orphanage.about}</p>
+            </div>
+
+            <div className="detail-page-pictures">
+              <div id="detail-page-grid">
+                {orphanage.pictures.map((image, index) => {
+                  return (
+                    <button
+                      key={image._id}
+                      className={activeImageIndex === index ? "active" : ""}
+                      type="button"
+                      onClick={() => {
+                        setActivateImageIndex(index);
+                      }}
+                    >
+                      <img
+                        src={`/uploads/${image.filename}`}
+                        alt={orphanage.name}
+                      />
+                    </button>
+                  );
+                })}
+              </div>
+              <button
+                className="detail-page-pass-pictures-right"
+                onClick={() => {
+                  const container = document.getElementById("detail-page-grid");
+                  const widthPicture = 0.16607 * window.screen.width;
+                  if (container)
+                    sideScroll(container, "right", 10, widthPicture, 10);
+                  if (activeImageIndex < orphanage.pictures.length - 1)
+                    setActivateImageIndex(activeImageIndex + 1);
+                }}
+              >
+                <FiArrowRight className="detail-page-pass-pictures-arrow" />
+              </button>
+              <button
+                className="detail-page-pass-pictures-left"
+                onClick={() => {
+                  const container = document.getElementById("detail-page-grid");
+                  const widthPicture = 0.16607 * window.screen.width;
+                  if (container)
+                    sideScroll(container, "left", 10, widthPicture, 10);
+                  if (activeImageIndex > 0)
+                    setActivateImageIndex(activeImageIndex - 1);
+                }}
+              >
+                <FiArrowLeft className="detail-page-pass-pictures-arrow" />
+              </button>
+            </div>
+
+            <div className="detail-page-map-container">
+              <MapContainer
+                className="detail-page-map"
+                center={[orphanage.latitude, orphanage.longitude]}
+                zoom={16}
+                attributionControl={false}
+              >
+                <TileLayer
+                  url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                />
+                <Marker
+                  interactive={false}
+                  icon={mapIcon}
+                  position={[orphanage.latitude, orphanage.longitude]}
+                />
+              </MapContainer>
+
+              <footer className="detail-page-footer-map">
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href={`https://www.google.com/maps/dir/?api=1&destination=${orphanage.latitude},${orphanage.longitude}`}
+                >
+                  Ver rotas no Google Maps
+                </a>
+              </footer>
+            </div>
+
+            <div className="detail-page-intructions">
+              <h2>Instruções para visita</h2>
+              <p>{orphanage.instructions}</p>
+            </div>
+
+            <div className="detail-page-time-visit">
+              <div className="detail-page-box detail-page-time">
+                <FiClock className="detail-page-time-icon" color="#15b6d6" />
+                Horário de visitas <br />
+                {orphanage.opening_hours}
+              </div>
+
+              {orphanage.open_on_weekends ? (
+                <div className="detail-page-box detail-page-open-on-weekends">
+                  <FiInfo
+                    className="detail-page-on-weekends-icon"
+                    color="#39CC83"
+                  />
+                  Atendemos <br />
+                  fim de semana
+                </div>
+              ) : (
+                <div className="detail-page-box detail-page-open-on-weekends-off">
+                  <FiInfo
+                    className="detail-page-off-weekends-icon"
+                    color="#FF669D"
+                  />
+                  Não Atendemos <br />
+                  fim de semana
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
