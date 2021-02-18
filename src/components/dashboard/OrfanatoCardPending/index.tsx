33c78855e1d70b7990c7001d67486cda89fbb39d
die-goncalves <die.goncalves1990@gmail.com>
2021-02-18 19:09:32 -0300

    Update the OrphanageCardPending component
    -> Make the map non-interactive, show only the location of the orphanage centralized and with fixed zoom;
    -> Show the name of the orphanage;
    -> Redirect the orphanage confirmation to the route /dashboard/pending/:id.

--- src/components/dashboard/OrfanatoCardPending/index.tsx
+++ src/components/dashboard/OrfanatoCardPending/index.tsx
@@ -1,33 +1,48 @@
 import React from "react";
 import { HiArrowRight } from "react-icons/hi";
-import { MapContainer, TileLayer } from "react-leaflet";
-
+import { MapContainer, Marker, TileLayer } from "react-leaflet";
+import { Link } from "react-router-dom";
+import { OrphanageParams } from "../../../interfaces/orphanages";
+import mapIcon from "../../../utils/mapIcon";
 import "./styles.css";
 
-function OrfanatoCardPending() {
+function OrfanatoCardPending({ orphanage }: OrphanageParams) {
   return (
     <div id="orfanato-card-pending">
       <div className="orfanato-card-pending-div-map">
         <MapContainer
-          center={[8.3693515, -6.8544287]}
+          center={[orphanage.latitude, orphanage.longitude]}
           zoom={3.45}
           className={"orfanato-card-pending-map"}
+          dragging={false}
+          touchZoom={false}
+          zoomControl={false}
+          scrollWheelZoom={false}
+          doubleClickZoom={false}
           attributionControl={false}
         >
           <TileLayer
             url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           />
+          <Marker
+            interactive={false}
+            icon={mapIcon}
+            position={[orphanage.latitude, orphanage.longitude]}
+          />
         </MapContainer>
       </div>
       <div className="orfanato-card-pending-options">
         <div className="orfanato-card-pending-name">
-          <p>Nome curto</p>
+          <p>{orphanage.name}</p>
         </div>
         <div className="orfanato-card-pending-buttons">
-          <a href="/" className="orfanato-card-pending-decision-link">
+          <Link
+            to={`/dashboard/pending/${orphanage._id}`}
+            className="orfanato-card-pending-decision-link"
+          >
             <HiArrowRight className="orfanato-card-pending-decision-icon" />
-          </a>
+          </Link>
         </div>
       </div>
     </div>
