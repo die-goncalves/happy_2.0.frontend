03873f506c29211040ffe6ee29f5d0d0d9c13bf5
die-goncalves <die.goncalves1990@gmail.com>
2021-02-18 19:18:23 -0300

    Update the pending orphanages page
    -> Search the backend for pending orphanages;
    -> Show the number of pending orphanages;
    -> Use the 'map' method so that each orphanage is provided to the OrfanatoCardPending component.

--- src/screens/dashboard/Pending/index.tsx
+++ src/screens/dashboard/Pending/index.tsx
@@ -1,29 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SidebarDashboard from "../../../components/dashboard/SidebarDashboard";
 import OrfanatoCardPending from "../../../components/dashboard/OrfanatoCardPending";
+import api from "../../../services/api";
+import { Orphanage } from "../../../interfaces/orphanages";
+
 import "./styles.css";
 
 function Pendingpage() {
+  const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
+
+  useEffect(() => {
+    api.get("hosting/pending").then((response) => {
+      setOrphanages(response.data);
+    });
+  }, []);
+
   return (
     <div id="pending-page">
       <SidebarDashboard />
       <div id="pending-page-main">
         <div className="pending-page-header">
           <p>Cadastros pendentes</p>
-          <span>x orfanatos</span>
+          <span>{orphanages.length} orfanatos</span>
         </div>
         <div className="pending-page-cluster">
           <div className="pending-page-grid-container">
-            <OrfanatoCardPending />
-            <OrfanatoCardPending />
-            <OrfanatoCardPending />
-            <OrfanatoCardPending />
-            <OrfanatoCardPending />
-            <OrfanatoCardPending />
-            <OrfanatoCardPending />
-            <OrfanatoCardPending />
-            <OrfanatoCardPending />
-            <OrfanatoCardPending />
+            {orphanages.map((element) => {
+              return (
+                <OrfanatoCardPending key={element._id} orphanage={element} />
+              );
+            })}
           </div>
         </div>
       </div>
