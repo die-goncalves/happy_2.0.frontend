b3b0da5e4e28724350ae18f10f529e5cddab6604
die-goncalves <die.goncalves1990@gmail.com>
2021-02-19 23:28:36 -0300

    Update the registered orphanages page
    -> Search the backend for all orphanages with 'pending=false';
    -> Inform the number of registered orphanages;
    -> Provide the OrfanatoCard component with each orphanage found and list on the page.

--- src/screens/dashboard/Registered/index.tsx
+++ src/screens/dashboard/Registered/index.tsx
@@ -1,30 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SidebarDashboard from "../../../components/dashboard/SidebarDashboard";
 import OrfanatoCard from "../../../components/dashboard/OrfanatoCard";
+import api from "../../../services/api";
+import { Orphanage } from "../../../interfaces/orphanages";
+
 import "./styles.css";
 
 function Registeredpage() {
+  const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
+
+  useEffect(() => {
+    api.get("hosting").then((response) => {
+      setOrphanages(response.data);
+    });
+  }, []);
+
   return (
     <div id="registered-page">
       <SidebarDashboard />
       <div id="registered-page-main">
         <div className="registered-page-header">
           <p>Orfanatos cadastrados</p>
-          <span>x orfanatos</span>
+          <span>{orphanages.length} orfanatos</span>
         </div>
         <div className="registered-page-cluster">
           <div className="registered-page-grid-container">
-            <OrfanatoCard />
-            <OrfanatoCard />
-            <OrfanatoCard />
-            <OrfanatoCard />
-            <OrfanatoCard />
-            <OrfanatoCard />
-            <OrfanatoCard />
-            <OrfanatoCard />
-            <OrfanatoCard />
-            <OrfanatoCard />
-            <OrfanatoCard />
+            {orphanages.map((element) => {
+              return <OrfanatoCard key={element._id} orphanage={element} />;
+            })}
           </div>
         </div>
       </div>
