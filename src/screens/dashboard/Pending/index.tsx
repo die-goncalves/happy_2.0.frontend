4e035ae23662a5ae429f72611fcceb49a8368e13
die-goncalves <die.goncalves1990@gmail.com>
2021-02-19 22:51:35 -0300

    Update pending orphanages page
    When there is no pending orphanage show icon and text indicating that there is no pending orphanage. Before it showed nothing.

--- src/screens/dashboard/Pending/index.tsx
+++ src/screens/dashboard/Pending/index.tsx
@@ -3,6 +3,7 @@ import SidebarDashboard from "../../../components/dashboard/SidebarDashboard";
 import OrfanatoCardPending from "../../../components/dashboard/OrfanatoCardPending";
 import api from "../../../services/api";
 import { Orphanage } from "../../../interfaces/orphanages";
+import zeroOrphanages from "../../../images/zero-orphanages.svg";
 
 import "./styles.css";
 
@@ -14,6 +15,28 @@ function Pendingpage() {
       setOrphanages(response.data);
     });
   }, []);
+
+  if (orphanages.length === 0) {
+    return (
+      <div id="pending-page">
+        <SidebarDashboard />
+
+        <div id="pending-page-main-zero">
+          <div className="pending-page-header-zero">
+            <p>Cadastros pendentes</p>
+          </div>
+          <div className="pending-page-zero-orphanages">
+            <div className="pending-page-zero-orphanages-image">
+              <img src={zeroOrphanages} alt="no-orphanages" />
+            </div>
+            <div className="pending-page-zero-orphanages-text">
+              <p>Nenhum no momento</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div id="pending-page">
