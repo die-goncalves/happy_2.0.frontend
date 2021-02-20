9f9170b1f98f542066f3905caad68e3219d401f9
die-goncalves <die.goncalves1990@gmail.com>
2021-02-19 23:23:00 -0300

    Update the OrfanatoCard component
    Create the 'orphanage' property in the component to receive an orphanage that will be provided by the registered orphanages page;
    Make the map non-interactive, show only the location of the orphanage centralized and with fixed zoom;
    Show the name of the orphanage;
    Redirect the orphanage for two routes:
    -> to update: /dashboard/registered/update/${orphanage._id}
    -> to exclude: /dashboard/registered/delete/${orphanage._id}

--- src/components/dashboard/OrfanatoCard/index.tsx
+++ src/components/dashboard/OrfanatoCard/index.tsx
@@ -1,38 +1,57 @@
 import React from "react";
 import { FiTrash2 } from "react-icons/fi";
 import { AiOutlineEdit } from "react-icons/ai";
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, TileLayer } from "react-leaflet";
+import { OrphanageParams } from "../../../interfaces/orphanages";
+import mapIcon from "../../../utils/mapIcon";
+import { Link } from "react-router-dom";
 
 import "./styles.css";
 
-function OrfanatoCard() {
+function OrfanatoCard({ orphanage }: OrphanageParams) {
   return (
     <div id="orfanato-card">
       <div className="orfanato-card-div-map">
         <MapContainer
-          center={[8.3693515, -6.8544287]}
+          center={[orphanage.latitude, orphanage.longitude]}
           zoom={3.45}
           className={"orfanato-card-map"}
+          dragging={false}
+          touchZoom={false}
+          zoomControl={false}
+          scrollWheelZoom={false}
+          doubleClickZoom={false}
           attributionControl={false}
         >
           <TileLayer
             url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           />
+          <Marker
+            interactive={false}
+            icon={mapIcon}
+            position={[orphanage.latitude, orphanage.longitude]}
+          />
         </MapContainer>
       </div>
       <div className="orfanato-card-options">
         <div className="orfanato-card-name">
-          <p>Nome do orfanato muito grande</p>
+          <p>{orphanage.name}</p>
         </div>
         <div className="orfanato-card-buttons">
-          <a href="/" className="orfanato-card-edit-link">
+          <Link
+            to={`/dashboard/registered/update/${orphanage._id}`}
+            className="orfanato-card-edit-link"
+          >
             <AiOutlineEdit className="orfanato-card-edit-icon" />
-          </a>
+          </Link>
 
-          <a href="/" className="orfanato-card-delete-link">
+          <Link
+            to={`/dashboard/registered/delete/${orphanage._id}`}
+            className="orfanato-card-delete-link"
+          >
             <FiTrash2 className="orfanato-card-delete-icon" />
-          </a>
+          </Link>
         </div>
       </div>
     </div>
@@ -40,37 +59,3 @@ function OrfanatoCard() {
 }
 
 export default OrfanatoCard;
-
-// <div className="map-do">
-//               <label
-//                 className={`${
-//                   !latlng && errors.latitude ? "do invalid" : "do valid"
-//                 }`}
-//               >
-//                 <span>Clique no mapa para adicionar a localização</span>
-//               </label>
-//               <div className="error">
-//                 {!latlng && errors.latitude ? (
-//                   <p className="error-message">{errors.latitude.message}</p>
-//                 ) : null}
-//               </div>
-
-//               <input
-//                 className="coordenada"
-//                 name="latitude"
-//                 autoComplete="off"
-//                 ref={register({
-//                   required:
-//                     "Procure onde está o orfanato e clique para marcar sua localização",
-//                 })}
-//               />
-//               <input
-//                 className="coordenada"
-//                 name="longitude"
-//                 autoComplete="off"
-//                 ref={register({
-//                   required:
-//                     "Procure onde está o orfanato e clique para marcar sua localização",
-//                 })}
-//               />
-//             </div>
