8b075be0b579e63c73cab9dbc0250f8d685364dd
die-goncalves <die.goncalves1990@gmail.com>
2021-02-24 23:00:22 -0300

    Authenticated user skip login page
    When the user is authenticated, he does not need to go through the login
    page.

--- src/screens/Homepage/index.tsx
+++ src/screens/Homepage/index.tsx
@@ -1,9 +1,23 @@
 import React from "react";
 import "./styles.css";
 import { FiArrowRight, FiKey } from "react-icons/fi";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import { useAuth } from "../../context/auth-context";
 
 function Homepage() {
+  const history = useHistory();
+  const { isLoggedIn, isLoading } = useAuth();
+
+  function isLogged() {
+    if (isLoading === false) {
+      if (isLoggedIn === true) {
+        history.push("/dashboard/registered");
+      } else {
+        history.push("/dashboard/login");
+      }
+    }
+  }
+
   return (
     <div id="home-page">
       <div className="grid-container">
@@ -13,10 +27,14 @@ function Homepage() {
           <span>Espírito Santo</span>
         </div>
         <div className="restrict">
-          <Link to="/dashboard/login" className="home-page-box-key">
+          <button
+            type="button"
+            className="home-page-box-key"
+            onClick={isLogged}
+          >
             <span>Acesso restrito</span>
             <FiKey className="home-page-key" color="rgba(255, 214, 102, 1.0)" />
-          </Link>
+          </button>
         </div>
         <div className="map">
           <Link to="/map" className="home-page-box-arrow">
