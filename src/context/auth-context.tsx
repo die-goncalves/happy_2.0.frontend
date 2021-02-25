31afa3c810fd12a9f864688d32d2cf44b9b0160d
die-goncalves <die.goncalves1990@gmail.com>
2021-02-24 22:08:05 -0300

    Update authentication context
    -> Add isLoggedIn Boolean state to indicate whether the user is authenticated;
    -> Add isLoading boolean state to indicate that the end of the operation has arrived. It serves to wait until you load the token and decide if it is authenticated or not, that is, a definitive isLoggedIn;
    -> Add function that performs the log out;
    -> In the log in function, set the key '@Happy: token' and its value in the browser localStorage.

--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -1,29 +1,68 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 import { RouteProps } from "react-router-dom";
 import api from "../services/api";
+import jwt_decode from "jwt-decode";
 
 interface AuthContextData {
   isLoggedIn: boolean;
+  isLoading: boolean;
   logIn(data: FormData): Promise<void>;
+  logOut(): void;
 }
 
-const AuthContext = createContext<AuthContextData>({} as AuthContextData);
+interface Decode {
+  _id: any;
+  iat: number;
+  exp: number;
+}
+
+export const AuthContext = createContext<AuthContextData>(
+  {} as AuthContextData
+);
 
 export const AuthProvider = ({ children }: RouteProps) => {
-  const [isAuthorized, setIsAuthorized] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const token = localStorage.getItem("@Happy: token");
+    if (token) {
+      const decoded: Decode = jwt_decode(token);
+      const current_time = new Date().getTime() / 1000;
+      if (current_time < decoded.exp) {
+        api.defaults.headers["authorization"] = `Bearer ${token}`;
+        setIsLoggedIn(true);
+        setIsLoading(false);
+      } else {
+        logOut();
+      }
+    } else {
+      setIsLoading(false);
+    }
+  }, []);
 
   async function logIn(data: FormData) {
     const response = await api.post("/user/authenticate", data);
 
     api.defaults.headers["authorization"] = `Bearer ${response.data.token}`;
-    setIsAuthorized(true);
+    localStorage.setItem("@Happy: token", response.data.token);
+    setIsLoggedIn(true);
+  }
+
+  function logOut() {
+    setIsLoggedIn(false);
+    setIsLoading(false);
+    const keysToRemove = ["@Happy: token"];
+    keysToRemove.forEach((item) => localStorage.removeItem(item));
   }
 
   return (
     <AuthContext.Provider
       value={{
-        isLoggedIn: isAuthorized,
+        isLoggedIn,
+        isLoading,
         logIn,
+        logOut,
       }}
     >
       {children}
